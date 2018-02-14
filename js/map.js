e8c14fd4119df9b4ba16fd8aab6ec537670d9163
lozjuan <lozjuan@gmail.com>
2018-02-14 12:40:01 +0100

    Add WB indicator geoJson

--- js/map.js
+++ js/map.js
@@ -3,14 +3,20 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>'
 
-var streets = L.tileLayer(mbUrl, { id: 'mapbox.streets', attribution: mbAttr }),
-    light = L.tileLayer(mbUrl, { id: 'mapbox.light', attribution: mbAttr });
+var streets = L.tileLayer(mbUrl, {
+        id: 'mapbox.streets',
+        attribution: mbAttr
+    }),
+    light = L.tileLayer(mbUrl, {
+        id: 'mapbox.light',
+        attribution: mbAttr
+    });
 
 var map = L.map('map', {
     center: [16.07, -37.38],
     zoom: 2,
     layers: [streets, light]
-    });
+});
 
 var pointStyle = {
     radius: 3,
@@ -21,16 +27,33 @@ var pointStyle = {
     fillOpacity: 1
 };
 
-function getGDPClass(gdp) {
-    return gdp > 1500000 ? '#800026' :
-        gdp > 1000000 ? '#BD0026' :
-        gdp > 600000 ? '#E31A1C' :
-        gdp > 300000 ? '#FC4E2A' :
-        gdp > 150000 ? '#FD8D3C' :
-        gdp > 50000 ? '#FEB24C' :
-        gdp > 20000 ? '#FED976' :
+function population(p) {
+    return p > 1300000000 ? '#800026' :
+        p > 1100000000 ? '#BD0026' :
+        p > 90000000 ? '#E31A1C' :
+        p > 60000000 ? '#FC4E2A' :
+        p > 30000000 ? '#FD8D3C' :
+        p > 10000000 ? '#FEB24C' :
+        p > 5000000 ? '#FED976' :
         '#FFEDA0';
 }
+
+function getGDP(gdp) {
+    return gdp > 10000000000000 ? '#800026' :
+        gdp > 1000000000000 ? '#BD0026' :
+        gdp > 100000000000 ? '#E31A1C' :
+        gdp > 10000000000 ? '#FC4E2A' :
+        gdp > 1000000000 ? '#FD8D3C' :
+        gdp > 100000000 ? '#FEB24C' :
+        gdp > 10000000 ? '#FED976' :
+        '#FFEDA0';
+}
+
+function getPopulation(p) {
+    if (p > 1000000) {
+        return radius = p / 1000000 * 3
+    }
+};
 
 var capital_sa = new L.geoJson(capital_sa, {
     onEachFeature: function(feature, layer) {
@@ -50,10 +73,8 @@ var capital_sa = new L.geoJson(capital_sa, {
         return L.circleMarker(latlng, pointStyle);
     },
     style: function(feature) {
-        if (feature.properties.population > 1000000) {
-            return {
-                radius: feature.properties.population / 1000000 * 3
-            }
+        return {
+            radius: getPopulation(feature.properties.population)
         }
     }
 });
@@ -74,10 +95,8 @@ var capital_na = new L.geoJson(capital_na, {
         return L.circleMarker(latlng, pointStyle);
     },
     style: function(feature) {
-        if (feature.properties.population > 1000000) {
-            return {
-                radius: feature.properties.population / 1000000 * 3
-            }
+        return {
+            radius: getPopulation(feature.properties.population)
         }
     }
 });
@@ -98,12 +117,11 @@ var capital_eu = new L.geoJson(capital_eu, {
         return L.circleMarker(latlng, pointStyle);
     },
     style: function(feature) {
-        if (feature.properties.population > 1000000) {
-            return {
-                radius: feature.properties.population / 1000000 * 3
-            }
+        return {
+            radius: getPopulation(feature.properties.population)
         }
     }
+
 });
 var capital_af = new L.geoJson(capital_af, {
     onEachFeature: function(feature, layer) {
@@ -122,12 +140,12 @@ var capital_af = new L.geoJson(capital_af, {
         return L.circleMarker(latlng, pointStyle);
     },
     style: function(feature) {
-        if (feature.properties.population > 1000000) {
-            return {
-                radius: feature.properties.population / 1000000 * 3
-            }
+        return {
+            radius: getPopulation(feature.properties.population)
         }
     }
+
+
 });
 var capital_as = new L.geoJson(capital_as, {
     onEachFeature: function(feature, layer) {
@@ -146,11 +164,11 @@ var capital_as = new L.geoJson(capital_as, {
         return L.circleMarker(latlng, pointStyle);
     },
     style: function(feature) {
-        if (feature.properties.population > 1000000) {
-            return {
-                radius: feature.properties.population / 1000000 * 3
-            }
+        return {
+            radius: getPopulation(feature.properties.population)
         }
+
+
     }
 
 });
@@ -171,18 +189,18 @@ var capital_oc = new L.geoJson(capital_oc, {
         return L.circleMarker(latlng, pointStyle);
     },
     style: function(feature) {
-        if (feature.properties.population > 1000000) {
-            return {
-                radius: feature.properties.population / 1000000 * 3
-            }
+        return {
+            radius: getPopulation(feature.properties.population)
         }
+
+
     }
 });
 var capital_an = new L.geoJson(capital_an, {
     onEachFeature: function(feature, layer) {
         layer.bindPopup('<h1>' + feature.properties.name + '</h1><p>population: ' + feature.properties.population + '</p>', {
             closeButton: false,
-            offset: L.point(0, -20)
+            offset: L.point(-0, -20)
         });
         layer.on('mouseover', function() {
             layer.openPopup();
@@ -195,68 +213,95 @@ var capital_an = new L.geoJson(capital_an, {
         return L.circleMarker(latlng, pointStyle);
     },
     style: function(feature) {
-        if (feature.properties.population > 1000000) {
-            return {
-                radius: feature.properties.population / 1000000 * 3
-            }
+        return {
+            radius: getPopulation(feature.properties.population)
         }
+
+
     }
 });
-
 var world = new L.geoJson(world, {
     style: function(feature) {
         return {
-            fillColor: getGDPClass(feature.properties.gdp_md_est),
-            weight: 2,
+            fillColor: population(feature.properties.pop_est),
+            weight: 1,
             opacity: 1,
-            color: 'white',
-            dashArray: '3',
+            color: "black",
             fillOpacity: 0.7
         }
     }
 });
 
-$.getJSON("data/world.geojson", function(data){
-    world.addData(data);
+var wb_gdp = new L.geoJson(wb_gdp, {
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup('<h1>' + feature.properties.country + '</h1><p>GDP: ' + feature.properties.value + '<br>year: ' + feature.properties.year + '</p>', {
+            closeButton: false,
+            offset: L.point(0, -20)
+        });
+        layer.on('mouseover', function() {
+            layer.openPopup();
+        });
+        layer.on('mouseout', function() {
+            layer.closePopup();
+        });
+    },
+    style: function(feature) {
+        return {
+            fillColor: getGDP(feature.properties.value),
+            weight: 1,
+            opacity: 1,
+            color: 'black',
+            fillOpacity: 0.7
+        }
+    }
 });
 
-$.getJSON("data/capital_SA.geojson", function(data){
-    capital_sa.addData(data).addTo(map);
+
+$.getJSON("data/capital_SA.geojson", function(data) {
+    capital_sa.addData(data)
 });
 
-$.getJSON("data/capital_NA.geojson", function(data){
-    capital_na.addData(data).addTo(map);
+$.getJSON("data/capital_NA.geojson", function(data) {
+    capital_na.addData(data)
 });
 
-$.getJSON("data/capital_EU.geojson", function(data){
-    capital_eu.addData(data).addTo(map);
+$.getJSON("data/capital_EU.geojson", function(data) {
+    capital_eu.addData(data)
 });
 
-$.getJSON("data/capital_AF.geojson", function(data){
-    capital_af.addData(data).addTo(map);
+$.getJSON("data/capital_AF.geojson", function(data) {
+    capital_af.addData(data)
 });
 
-$.getJSON("data/capital_AS.geojson", function(data){
-    capital_as.addData(data).addTo(map);
+$.getJSON("data/capital_AS.geojson", function(data) {
+    capital_as.addData(data)
 });
 
-$.getJSON("data/capital_OC.geojson", function(data){
-    capital_oc.addData(data).addTo(map)
+$.getJSON("data/capital_OC.geojson", function(data) {
+    capital_oc.addData(data)
 });
 
-$.getJSON("data/capital_AN.geojson", function(data){
-    capital_an.addData(data).addTo(map);
+$.getJSON("data/capital_AN.geojson", function(data) {
+    capital_an.addData(data)
+});
+
+$.getJSON("data/wb_gdp.json", function(data) {
+    wb_gdp.addData(data)
+});
+$.getJSON("data/world.geojson", function(data) {
+    world.addData(data)
 });
 
 var overlays = {
     "South america": capital_sa,
     "North america": capital_na,
     "Europe": capital_eu,
-    "Afrique": capital_af,
+    "Africa": capital_af,
     "Asia": capital_as,
     "Oceania": capital_oc,
-    "Antartica": capital_an,
-    "Countries boundaries": world
+    "Antarctica": capital_an,
+    "GDP World Bank": wb_gdp,
+    "Country population": world
 }
 var baseLayers = {
     "Light": light,
--- python/get_geonames_data.py
+++ python/get_geonames_data.py
@@ -1,9 +1,13 @@
 import requests
 import geojson
+import json
 
 url = "http://api.geonames.org/searchJSON?featureCode=PPLC&continentCode=AN&username=lozjuan"
 result = requests.get(url)
 data = result.json()
+url2 = "http://api.worldbank.org/v2/countries/all/indicators/NY.GDP.MKTP.CD?date=2002&format=json&per_page=300"
+wb = requests.get(url2)
+wb_data = wb.json()
 
 
 def geonames_to_geojson_point(outfile):
@@ -11,6 +15,29 @@ def geonames_to_geojson_point(outfile):
     capitals = []
     for i in data['geonames']:
         capitals.append(geojson.Feature(geometry=geojson.Point((round(float(i['lng']), 1), round(float(i['lat']), 1))),
-                                        properties={'name': i['name'], 'country': i['countryName'], 'population': i['population']}))
+                                        properties={'name': i['name'], 'country': i['countryName'],
+                                                    'population': i['population']}))
     with open(outfile, 'w') as file:
         geojson.dump(geojson.FeatureCollection(capitals), file)
+
+
+def wb_indicator_to_geojson(outfile):
+    """converts json data from world bank api to geojson. Outfile argument is a filepath"""
+    countries = []
+    with open("../data/world.geojson", 'r', encoding='utf8') as world:
+        world = json.load(world)
+    for c in world['features']:
+        for c1 in wb_data[1]:
+            if c1['country']['id'] == c['properties']['iso_a2'] and c['geometry']['type'] == "MultiPolygon":
+                countries.append(geojson.Feature(geometry=geojson.MultiPolygon(c['geometry']['coordinates']),
+                                                 properties={'country': c1['country']['value'],
+                                                             'indicator': c1['indicator']['value'],
+                                                             'value': c1['value'], 'year': c1['date']}))
+            elif c1['country']['id'] == c['properties']['iso_a2'] and c['geometry']['type'] == "Polygon":
+                countries.append(geojson.Feature(geometry=geojson.Polygon(c['geometry']['coordinates']),
+                                                 properties={'country': c1['country']['value'],
+                                                             'indicator': c1['indicator']['value'],
+                                                             'value': c1['value'], 'year': c1['date']}))
+
+    with open(outfile, 'w') as file:
+        geojson.dump(geojson.FeatureCollection(countries), file)

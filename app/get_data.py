a04b95fa205546724b66deb755e29e81cbea30a9
lozjuan <lozjuan@gmail.com>
2018-03-02 19:47:03 +0100

    DataMapperV1

--- app/get_data.py
+++ app/get_data.py
@@ -6,14 +6,6 @@ from app import app
 from pandas.io.json import json_normalize
 
 
-
-@app.after_request
-def add_header(response):
-    """Avoid cache Json files to be saved in browser in order to display data dynamically"""
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0"
-    return response
-
-
 def geonames_to_geojson_point(outfile):
     """converts json data from geonames api to geojson. Outfile argument is a filepath"""
     continents = ['SA', 'NA', 'EU', 'OC', 'AS', 'AN', 'AF']
@@ -39,19 +31,24 @@ def wb_indicator_to_geojson_point(ind_code, year):
     wb_raw = requests.get(data_source)
     wb_json = wb_raw.json()
     capital = []
-    with open('app/static/data/capitals.json', 'r', encoding='utf8') as source:
-        capitals = json.load(source)
-    for c in capitals['features']:
-        for c1 in wb_json[1]:
-            if c1['country']['value'] == c['properties']['country']:
-                capital.append(geojson.Feature(geometry=geojson.Point(c['geometry']['coordinates']),
-                                               properties={'country': c1['country']['value'],
-                                                           'value': c1['value'],
-                                                           'year': c1['date'],
-                                                           'indicator': c1['indicator']['value']}))
-    with open('app/static/data/wb_ind_point.json', 'w') as file:
-        geojson.dump(geojson.FeatureCollection(capital), file)
-    create_wb_legend_values('app/static/data/ind_legend_point.json', ind_code, year)
+    df = json_normalize(wb_json[1])
+    if df['value'].dropna().empty:
+        message = 'Data not available'
+        return message
+    else:
+        with open('app/static/data/capitals.json', 'r', encoding='utf8') as source:
+            capitals = json.load(source)
+        for c in capitals['features']:
+            for c1 in wb_json[1]:
+                if c1['country']['value'] == c['properties']['country']:
+                    capital.append(geojson.Feature(geometry=geojson.Point(c['geometry']['coordinates']),
+                                                   properties={'country': c1['country']['value'],
+                                                               'value': c1['value'],
+                                                               'year': c1['date'],
+                                                               'indicator': c1['indicator']['value']}))
+        with open('app/static/data/wb_ind_point.json', 'w') as file:
+            geojson.dump(geojson.FeatureCollection(capital), file)
+        create_wb_legend_values('app/static/data/ind_legend_point.json', ind_code, year)
 
 
 def wb_indicator_to_geojson_polygon(ind_code, year):
@@ -61,28 +58,33 @@ def wb_indicator_to_geojson_polygon(ind_code, year):
                   "{0}?date={1}&format=json&per_page=300".format(ind_code, year)
     wb_raw = requests.get(data_source)
     wb_json = wb_raw.json()
-    with open('app/static/data/world.geojson', 'r', encoding='utf8') as world:
-        world = json.load(world)
-    for c in world['features']:
-        for c1 in wb_json[1]:
-            if c1['country']['id'] == c['properties']['iso_a2'] and c['geometry']['type'] == "MultiPolygon":
-                countries.append(geojson.Feature(geometry=geojson.MultiPolygon(c['geometry']['coordinates']),
-                                                 properties={'country': c1['country']['value'],
-                                                             'indicator': c1['indicator']['value'],
-                                                             'value': c1['value'], 'year': c1['date']}))
-            elif c1['country']['id'] == c['properties']['iso_a2'] and c['geometry']['type'] == "Polygon":
-                countries.append(geojson.Feature(geometry=geojson.Polygon(c['geometry']['coordinates']),
-                                                 properties={'country': c1['country']['value'],
-                                                             'indicator': c1['indicator']['value'],
-                                                             'value': c1['value'], 'year': c1['date']}))
-    with open('app/static/data/wb_ind_polygon.json', 'w') as file:
-        geojson.dump(geojson.FeatureCollection(countries), file)
-    create_wb_legend_values('app/static/data/ind_legend_polygon.json', ind_code, year)
+    df = json_normalize(wb_json[1])
+    if df['value'].dropna().empty:
+        return 'Data not available'
+    else:
+        with open('app/static/data/world.geojson', 'r', encoding='utf8') as world:
+            world = json.load(world)
+        for c in world['features']:
+            for c1 in wb_json[1]:
+                if c1['country']['id'] == c['properties']['iso_a2'] and c['geometry']['type'] == "MultiPolygon":
+                    countries.append(geojson.Feature(geometry=geojson.MultiPolygon(c['geometry']['coordinates']),
+                                                     properties={'country': c1['country']['value'],
+                                                                 'indicator': c1['indicator']['value'],
+                                                                 'value': c1['value'], 'year': c1['date']}))
+                elif c1['country']['id'] == c['properties']['iso_a2'] and c['geometry']['type'] == "Polygon":
+                    countries.append(geojson.Feature(geometry=geojson.Polygon(c['geometry']['coordinates']),
+                                                     properties={'country': c1['country']['value'],
+                                                                 'indicator': c1['indicator']['value'],
+                                                                 'value': c1['value'], 'year': c1['date']}))
+        with open('app/static/data/wb_ind_polygon.json', 'w') as file:
+            geojson.dump(geojson.FeatureCollection(countries), file)
+        create_wb_legend_values('app/static/data/ind_legend_polygon.json', ind_code, year)
 
 
 def create_wb_legend_values(outfile, ind_code, year):
     """Calculates legend values and saves it as json"""
-    data_source = "http://api.worldbank.org/v2/countries/all/indicators/{0}?date={1}&format=json&per_page=300".format(ind_code, year)
+    data_source = "http://api.worldbank.org/v2/countries/all/indicators/{0}?date={1}&format=json&per_page=300".format(
+        ind_code, year)
     wb_raw = requests.get(data_source)
     wb_json = wb_raw.json()
     all_data = json_normalize(wb_json[1])
@@ -101,11 +103,12 @@ def get_indicator_codes(source, year):
     wb_raw = requests.get(ind_info)
     wb_json = wb_raw.json()
     all_indicator = json_normalize(wb_json[1])
-    wdi = all_indicator[all_indicator['source.value'] == source]
-    code_id = wdi['id'].to_frame()
+    db_source = all_indicator[all_indicator['source.value'] == source]
+    code_id = db_source['id'].to_frame()
     result = {}
     for c in code_id['id']:
-        ind_data = "http://api.worldbank.org/v2/countries/all/indicators/{0}?date={1}&format=json&per_page=300".format(c, year)
+        ind_data = "http://api.worldbank.org/v2/countries/all/indicators/{0}?date={1}&format=json&per_page=300".format(
+            c, year)
         wb_raw = requests.get(ind_data)
         wb_json = wb_raw.json()
         if wb_json[1] is not None:
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from werkzeug.utils import redirect
 from app import app
-from flask import render_template, request
+from flask import render_template, request, flash
 import json
 from app.get_data import wb_indicator_to_geojson_polygon, wb_indicator_to_geojson_point
 
-year = range(1990, 2018)
+year = list(range(1990, 2018))
 with open('app/static/data/wdi_codes.json', 'r') as codes:
     codes = json.load(codes)
 
@@ -15,14 +15,23 @@ def index():
     return render_template('index.html', title='Home', codes=codes, year=year)
 
 
+@app.after_request
+def add_header(response):
+    """Avoid cache Json files to be saved in browser in order to display data dynamically"""
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0"
+    return response
+
+
 @app.route('/get_polygon_indicator', methods=['GET', 'POST'])
 def get_polygon_indicator():
-    global year
     global codes
+    global year
     try:
         ind = request.form['polygon_ind']
-        year = request.form['polygon_year']
-        wb_indicator_to_geojson_polygon(ind, year)
+        date = request.form['polygon_year']
+        wb_indicator_to_geojson_polygon(ind, date)
+        if wb_indicator_to_geojson_polygon(ind, date) == 'Data not available':
+            return render_template('index.html', title='Home', codes=codes, year=year, error_pol='Data not available')
     except:
         error_pol = "Please choose an indicator and a year"
         return render_template('index.html', title='Home', codes=codes, year=year, error_pol=error_pol)
@@ -31,12 +40,14 @@ def get_polygon_indicator():
 
 @app.route('/get_point_indicator', methods=['GET', 'POST'])
 def get_point_indicator():
-    global year
     global codes
+    global year
     try:
         ind = request.form['point_ind']
-        year = request.form['point_year']
-        wb_indicator_to_geojson_point(ind, year)
+        date = request.form['point_year']
+        wb_indicator_to_geojson_point(ind, date)
+        if wb_indicator_to_geojson_point(ind, date) == 'Data not available':
+            return render_template('index.html', title='Home', codes=codes, year=year, error_point='Data not available')
     except:
         error_point = "Please choose an indicator and a year"
         return render_template('index.html', title='Home', codes=codes, year=year, error_point=error_point)
